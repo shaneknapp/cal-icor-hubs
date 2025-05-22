5de20a330ceb62a4062ffa12ceb205c22ca0b2d5
shane knapp <incomplete@gmail.com>
2025-05-22 13:59:08 -0700

    adding label and pr creation for github

--- scripts/create_deployment.py
+++ scripts/create_deployment.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import os
+import re
 import subprocess
 import sys
 from pathlib import Path
@@ -79,7 +80,153 @@ def handle_secrets(school_arg, env_arg):
     print("Secret file generation and encryption completed.")
 
 
-def create_deployment(config, root_path, manual_config=False):
+def create_label(hub_name, root_path):
+    """
+    Create labels for the new hub in the GitHub repository.
+
+    Args:
+        hub_name (str): The name of the hub.
+        root_path (str): The path to the root directory of the repository.
+    """
+    labeler_path = os.path.join(root_path, ".github", "labeler.yml")
+    hub_label = f"""
+'hub: {hub_name}':
+  - 'deployments/{hub_name}/**'
+""".strip()
+    with open(labeler_path, "a") as f:
+        print(hub_label, file=f)
+    print(f"Added {hub_name} to the labeler.yml file.")
+
+    # create the github label for the new hub
+    github_label = f"hub: {hub_name}"
+    try:
+        subprocess.run(
+            [
+                "gh",
+                "label",
+                "-Rcal-icor/cal-icor-hubs",
+                "create",
+                github_label,
+                "--description",
+                f"Label for {hub_name} deployment.",
+            ],
+            check=True,
+        )
+        print(f"Created GitHub label for {hub_name}.")
+    except subprocess.CalledProcessError as e:
+        print(f"Unable to get branch from {root_path}: {e}.")
+        exit(1)
+
+    return hub_label
+
+
+def create_pr(github_user, hub_name, root_path, branch_name, hub_label):
+    """
+    Add, commit and create a pull request for the new hub deployment.
+    Args:
+        github_user (str): The GitHub username of the user creating the pull request.
+        hub_name (str): The name of the hub.
+        root_path (str): The path to the root directory of the repository.
+    """
+    body = f"Add {hub_name} deployment."
+    remote = "origin"
+    title = f"Add {hub_name} deployment."
+    upstream_repo = "git@github.com:cal-icor/cal-icor-hubs.git"
+    changed_files = subprocess.run(
+        ["git", "status", "--porcelain"],
+        cwd=root_path,
+        capture_output=True,
+        text=True,
+    )
+
+    print(f"Adding all changes for {hub_name} to feature branch:")
+    print(changed_files.stdout.strip())
+
+    files_to_add = [
+        f"deployments/{hub_name}/",
+        ".github/labeler.yml",
+    ]
+    for file in files_to_add:
+        print(f"Adding {file} to staging.")
+        try:
+            subprocess.check_call(["git", "add", file], cwd=root_path)
+        except subprocess.CalledProcessError as e:
+            print(f"Error adding {file} to commit: {e}")
+            exit(1)
+
+    commit_message = f"Add {hub_name} deployment."
+    print(f"Committing changes for {hub_name} with message {commit_message}.")
+    try:
+        subprocess.check_call(
+            ["git", "commit", "-m", f"{commit_message}"], cwd=root_path
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Error committing {hub_name}: {e}")
+        exit(1)
+
+    print(f"Pushing {branch_name} to {remote}")
+    try:
+        subprocess.check_call(["git", "push", remote, branch_name], cwd=root_path)
+    except subprocess.CalledProcessError as e:
+        print(f"Error pushing {branch_name} to {remote}: {e}")
+        exit(1)
+
+    print(f"Creating a pull request for {hub_name} on branch {branch_name}")
+    owner_and_repo = re.search(".+:(.+?).git$", upstream_repo).group(1)
+    try:
+        command = [
+            "gh",
+            "pr",
+            "new",
+            f"-t {title}",
+            f"-R{owner_and_repo}",
+            f"-H{github_user}:{branch_name}",
+            "-Bstaging",
+            f"-l{hub_label}",
+        ]
+        if body is not None:
+            command.append(f"-b {body}")
+        print(command)
+        subprocess.run(command)
+    except subprocess.CalledProcessError as e:
+        print(f"Unable to create pull request for {hub_name}: {e}")
+        exit(1)
+
+
+def create_branch(branch_name, root_path):
+    """
+    Create a new branch in the Git repository.
+
+    Args:
+        branch_name (str): The name of the new branch to be created.
+    """
+    try:
+        branch = (
+            subprocess.run(
+                ["git", "branch", "--show-current"], cwd=root_path, capture_output=True
+            )
+            .stdout.decode("utf-8")
+            .strip()
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Unable to get branch from {root_path}: {e}.")
+        exit(1)
+
+    if branch != "staging":
+        print(
+            f"Currently not on branch 'staging' in {root_path}. Not "
+            + "creating a feature branch and exiting."
+        )
+        exit(1)
+    try:
+        subprocess.check_call(["git", "switch", "-c", branch_name], cwd=root_path)
+        print(f"Created new branch: {branch_name}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error creating branch {branch_name}: {e}")
+        exit(1)
+
+
+def create_deployment(config, github_user, root_path, manual_config=False):
     """
     Create a new deployment for an institution using the provided configuration.
     Args:
@@ -88,6 +235,11 @@ def create_deployment(config, root_path, manual_config=False):
         manual_config (bool): If True, the script will ask for confirmation for each step.
     """
 
+    # Create a feature branch
+    branch_name = f"add-{config['hub_name']}-deployment"
+    create_branch(branch_name, root_path)
+
+    # Run the cookiecutter to generate the deployment files
     cookiecutter(
         template=f"{root_path}/deployments/template",
         output_dir=f"{root_path}/deployments",
@@ -116,16 +268,11 @@ def create_deployment(config, root_path, manual_config=False):
     for env in ["prod", "staging"]:
         handle_secrets(config["hub_name"], env)
 
-    # add the new deployment to the github labeler
-    labeler_path = os.path.join(root_path, ".github", "labeler.yml")
-    hub_name = config["hub_name"]
-    hub_label = f"""
-'hub: {hub_name}':
-  - 'deployments/{hub_name}/**'
-""".strip()
-    with open(labeler_path, "a") as f:
-        print(hub_label, file=f)
-    print(f"Added {hub_name} to the labeler.yml file.")
+    # Create labels for the new hub
+    hub_label = create_label(config["hub_name"], root_path)
+
+    # Create a pull request for the new hub
+    create_pr(github_user, config["hub_name"], root_path, branch_name, hub_label)
 
 
 def main():
@@ -141,6 +288,13 @@ def main():
         + "You will also need to fill out the cookiecutter template config "
         + "in the _deploy_configs/<hubname>.yaml dir in the root of the repo.",
         formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        "--github_user",
+        "-g",
+        type=str,
+        help="The GitHub username of the user creating the pull request.",
+        required=True,
     )
     parser.add_argument(
         "institution_name",
@@ -188,7 +342,7 @@ def main():
         print(f"Error loading config for {args.institution_name}.")
         exit(1)
 
-    create_deployment(config, root_path, args.manual_config)
+    create_deployment(config, args.github_user, root_path, args.manual_config)
 
 
 if __name__ == "__main__":
