49caee3cd02138e217b85ca2fb4bc30cbeea44bc
shane knapp <incomplete@gmail.com>
2025-11-24 14:12:58 -0800

    the new labeler syntax is a terrible game of whack-a-mole

--- scripts/create_deployment.py
+++ scripts/create_deployment.py
@@ -94,8 +94,7 @@ def create_label(hub_name: str, root_path: str) -> str:
     labeler_path = os.path.join(root_path, ".github", "labeler.yml")
     hub_label = f"""
 'hub: {hub_name}':
-- changed-files:
-  - any-glob-to-any-file: 'deployments/{hub_name}/**'
+  - 'deployments/{hub_name}/**'
 """.strip()
     with open(labeler_path, "a") as f:
         print(hub_label, file=f)
